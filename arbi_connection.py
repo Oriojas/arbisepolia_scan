8843bea26ea1368b71319d3098a9c8549b01f858
oriojas <oscarriojas@gmail.com>
2024-05-08 16:48:59 -0500

    feat/ create and used endpoint

--- arbi_connection.py
+++ arbi_connection.py
@@ -6,7 +6,6 @@ class ArbiSepScanner(object):
     def __init__(self, key):
         self.key = key
 
-
     def transaction_address(self, address):
         url = (f"https://api-sepolia.arbiscan.io/api"
                f"?module=account"
@@ -17,6 +16,17 @@ class ArbiSepScanner(object):
                f"&page=1"
                f"&offset=10"
                f"&sort=asc"
+               f"&apikey={self.key}")
+
+        response = requests.get(url)
+
+        return response.json()
+
+    def transaction_hash(self, t_hash):
+        url = (f"https://api-sepolia.arbiscan.io/api"
+               f"?module=account"
+               f"&action=txlistinternal"
+               f"&txhash={t_hash}"
                f"&apikey={self.key}")
 
         response = requests.get(url)
---
+++ graph_data_hash.py
@@ -0,0 +1,46 @@
+import os
+import json
+import pandas as pd
+import arbi_connection as ab
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KEY = os.environ.get('KEY')
+
+df_faucet = pd.read_csv('/home/oscar/Github/arbisepolia_scan/data_raw/data_sepolia.csv',
+                        index_col=False)
+
+df_result = pd.DataFrame()
+
+for hash_id in df_faucet['Txhash']:
+    print(hash_id)
+    try:
+        temp_resp = ab.ArbiSepScanner(key=KEY).transaction_hash(t_hash=hash_id)
+    except:
+        temp_resp = {"status": "1",
+                     "message": "OK",
+                     "result": [
+                                    {
+                                        "blockNumber": "0",
+                                        "timeStamp": "0",
+                                        "from": "0",
+                                        "to": "0",
+                                        "value": "0",
+                                        "contractAddress": "0",
+                                        "input": "0",
+                                        "type": "0",
+                                        "gas": "0",
+                                        "gasUsed": "0",
+                                        "isError": "0",
+                                        "errCode": "0"
+                                    }
+                                ]
+                     }
+
+    df_temp = pd.DataFrame(temp_resp['result'])
+    print(df_temp)
+    df_result = pd.concat([df_result, df_temp])
+
+df_result.to_csv('/home/oscar/Github/arbisepolia_scan/output_data/txlistinternal.csv')
+
