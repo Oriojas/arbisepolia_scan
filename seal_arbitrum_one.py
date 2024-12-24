fa7706ab7616a5b4d7bdd72a13965ac52ce574a4
Oscar Riojas <oscarriojas@gmail.com>
2024-12-23 22:46:46 -0500

    feat/ test API

--- seal_arbitrum_one.py
+++ seal_arbitrum_one.py
@@ -1,4 +1,10 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = os.environ.get('ARBITRUM_URL')
 
 class ArbiOneScanner(object):
 
@@ -6,11 +12,16 @@ class ArbiOneScanner(object):
         self.key = key
 
 
-    def transaction_hash(self, hash):
-        url = (f"https://api.arbiscan.io/api"
+    def transaction_hash(self, address):
+        url = (f"{URL}"
                f"?module=account"
                f"&action=txlistinternal"
-               f"&txhash={hash}"
+               f"&address={address}"
+               f"&startblock=0"
+               f"&endblock=99999999"
+               f"&page=1"
+               f"&offset=1000"
+               f"&sort=asc"
                f"&apikey={self.key}")
 
         response = requests.get(url)
@@ -18,5 +29,16 @@ class ArbiOneScanner(object):
         return response.json()
 
 if __name__ == "__main__":
-    transaction = ArbiOneScanner(key="8DXKZIKYAHBCFZUN5IZM2VRNW7C89FS61I").transaction_hash(hash="0x5e2ce795c7c184ea26cc64550ae5f0c2cfd7ff14c6b932bac0b2eaa278ccc522")
+    import json
+
+    KEY = os.environ.get('KEY')
+    MAIN_DIR = os.environ.get('MAIN_DIR')
+    address_contract = "0x8c5ff04497062be94e59412163a2e771a8154beb"
+
+    transaction = ArbiOneScanner(key=KEY).transaction_hash(address=address_contract)
+
+    details_filename = os.path.join(f"{MAIN_DIR}/output_data", f"internal_tx_{address_contract}.json")
+    with open(details_filename, 'w', encoding='utf-8') as f:
+        json.dump(transaction, f, indent=4)
+
     print(transaction)
